9c525fe6e8f1cc4c0d7fe1dd250244f9b94c3cbf
siddhardhans2024 <162001368+siddhardhans2024@users.noreply.github.com>
2025-03-07 16:23:30 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import runpod
+import asyncio
+
+
+async def async_generator_handler(job):
+    input = job["input"]
+    count = input["n"]
+    for i in range(count):
+        # Generate an asynchronous output token
+        output = f"Generated async token output {i}"
+        yield output
+
+        # Simulate an asynchronous task, such as processing time for a large language model
+        await asyncio.sleep(1)
+
+
+# Configure and start the RunPod serverless function
+runpod.serverless.start(
+    {
+        "handler": async_generator_handler,  # Required: Specify the async handler
+        "return_aggregate_stream": True,  # Optional: Aggregate results are accessible via /run endpoint
+    }
+)
